983c562bb4d65944dc8836843f71206127fedb0d
Andrew Symington <simmers@google.com>
2026-07-23 17:12:42 +0000

    Add copyright headers to all source code.
    Signed-off-by: Andrew Symington <simmers@google.com>

--- waffle/render.py
+++ waffle/render.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright 2026 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """VIEW stage: renders the most recent "<date>_analyzed.yaml" file (the
 CONTROLLER stage's output) into a static HTML waffle board (site/index.html),
 grouped into Human / Unknown / Bot / AI tables in that order so
